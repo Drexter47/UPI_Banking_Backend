ff1c8054e39e83600f8cd36364b03ef3fac5bc94
Valay Andhariya <andhariyavalay4@gmail.com>
2024-05-14 22:58:44 +0530

    final commits

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ config({
 // );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*", process.env.FRONTEND_URL);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
